6d1df9fc9537efa54ef5109a60d77353293c87ce
Isabel Miskinich <isamiskinich@gmail.com>
2020-08-18 15:23:16 -0300

    first commit

---
+++ web/src/App.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Routes from './routes';
+
+import './assets/styles/global.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Routes />
+    );
+  }
+}
+
+export default App;
